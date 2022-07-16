767f7368664653ea4435cf622cc1b8cc3ffa7862
skay_4 <sarahkaywren@gmail.com>
2022-07-16 12:34:39 -0500

    m

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
-from flask import Flask, render_template
+from threading import local
+from flask import Flask, render_template,request
 from google.cloud import datastore
 
 app = Flask(__name__)
@@ -16,8 +17,8 @@ def store_time(dt):
 
 
 def fetch_times(limit):
-    query = datastore_client.query(kind='visit')
-    query.order = ['-timestamp']
+    query = datastore_client.query(kind='red_star_queues')
+    #query.order = ['-timestamp']
 
     times = query.fetch(limit=limit)
 
@@ -26,26 +27,20 @@ def fetch_times(limit):
 @app.route('/queue_add')
 def root():
     # Store the current access time in Datastore.
-    store_time(datetime.datetime.now(tz=datetime.timezone.utc))
-
+    #store_time(datetime.datetime.now(tz=datetime.timezone.utc))
+    args = request
+    print(args)
     # Fetch the most recent 10 access times from Datastore.
-    times = fetch_times(10)
+    limit=10
+    times = fetch_times(limit)
 
-    return render_template(
-        'index.html', times=times)
+    return times
+
+app.run(host='localhost',port=8080)
+
+'''
+data_structure={id:entity_id,
+                rs_level:rs_level,
+                users=[user1,user2,user3]}
+'''
             
-            
-            rs_level,users=use.check_ready()
-            if rs_level:
-                found=1
-                users_mentions=[]
-                for user in users:
-                    users_mentions.append('<@'+user+'>')
-                res_msg = str(users_mentions).strip("[']")+' ready for '+str(rs_level) +". Where ya bitches off to?",found
-        else:
-            print('found is not 0',found)
-            res_msg= "you're already in a queue for "+str(rs_active_queue)
-        if found==0:
-            await queue_init(ctx)
-        else:
-            await ctx.send(res_msg)
