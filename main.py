70f017c6204f62008d1f393dd844947b96da154c
skay_4 <sarahkaywren@gmail.com>
2022-07-16 12:12:41 -0500

    m

--- main.py
+++ main.py
@@ -33,3 +33,19 @@ def root():
 
     return render_template(
         'index.html', times=times)
+            
+            
+            rs_level,users=use.check_ready()
+            if rs_level:
+                found=1
+                users_mentions=[]
+                for user in users:
+                    users_mentions.append('<@'+user+'>')
+                res_msg = str(users_mentions).strip("[']")+' ready for '+str(rs_level) +". Where ya bitches off to?",found
+        else:
+            print('found is not 0',found)
+            res_msg= "you're already in a queue for "+str(rs_active_queue)
+        if found==0:
+            await queue_init(ctx)
+        else:
+            await ctx.send(res_msg)
