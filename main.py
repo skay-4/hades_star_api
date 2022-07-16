ac853d249143fdec0c8d812fa8e9daa912403c14
skay_4 <sarahkaywren@gmail.com>
2022-07-16 12:11:06 -0500

    m

---
+++ main.py
@@ -0,0 +1,35 @@
+import datetime
+from flask import Flask, render_template
+from google.cloud import datastore
+
+app = Flask(__name__)
+datastore_client = datastore.Client()
+
+
+def store_time(dt):
+    entity = datastore.Entity(key=datastore_client.key('visit'))
+    entity.update({
+        'timestamp': dt
+    })
+
+    datastore_client.put(entity)
+
+
+def fetch_times(limit):
+    query = datastore_client.query(kind='visit')
+    query.order = ['-timestamp']
+
+    times = query.fetch(limit=limit)
+
+    return times
+
+@app.route('/queue_add')
+def root():
+    # Store the current access time in Datastore.
+    store_time(datetime.datetime.now(tz=datetime.timezone.utc))
+
+    # Fetch the most recent 10 access times from Datastore.
+    times = fetch_times(10)
+
+    return render_template(
+        'index.html', times=times)
